7b83e2ff7a6927a6d0f884cbbdf8c2b2e497b5db
llamachoi <45895440+llamachoi@users.noreply.github.com>
2025-08-08 21:38:37 +0900

    blue 스테이지까지 완료

--- Assets/1.Scripts/PlayerController.cs
+++ Assets/1.Scripts/PlayerController.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float maxFallSpeed = -10f;
     public float rangeX;
     public SpriteRenderer pogoTipRenderer;
+    private Rigidbody2D rb;
 
     private void Start()
     {
         pogoTipRenderer.color = ColorManager.colors[0];
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -32,6 +35,15 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             ChangePogoColor();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Y속도가 maxFallSpeed보다 작아지면(더 빨라지면) 제한
+        if (rb.linearVelocity.y < maxFallSpeed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, maxFallSpeed);
         }
     }
 
